43b61f2fd2a20e4b436836eb29b80419e0075f94
Dylan Brown <40218657+dylanbr0wn@users.noreply.github.com>
2022-07-31 00:44:51 -0700

    fave

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -130,7 +130,24 @@ const BlogPage: NextPage<{
 				<meta property="twitter:title" content={pageTitle} />
 				<meta property="twitter:description" content={desc} />
 				<meta property="twitter:image" content={thumbnail}></meta>
-				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
 				<meta name="theme-color" content="#171717"></meta>
 			</Head>
 			<Header blogName={title} />
